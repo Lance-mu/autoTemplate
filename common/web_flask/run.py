bd2ad1eb453deceba16146ae3f0c4aa7f76a67ab
qianwulin <qianwulin@bytedance.com>
2022-10-21 15:20:49 +0800

    modify flask

--- common/web_flask/run.py
+++ common/web_flask/run.py
@@ -4,33 +4,38 @@
 Time    : 2022/8/5 11:45 下午
 Author  : qianwulin@bytedance.com
 """
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, url_for
 
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     data = {
         "status": "login success"
     }
-    if request.method == "GET":
-        return render_template('login.html')
     if request.method == "POST":
-        name = request.form.get("name")
-        password = request.form.get("password")
-        if name == "zhangsan" and password == "123":
+        # 取表单参数
+        f = request.form
+        name = f["username"]
+        password = f["password"]
+        if name == "admin" and password == "123456":
             return render_template('login.html', data=data)
-        else:
-            # raise 主动抛出异常
-            # abort函数，在网页当中抛出异常
-            abort(404)
-            return None
+        # raise 主动抛出异常、abort函数，在网页当中抛出异常
+        # abort(404)
+        return "账号或者密码错误"
+    else:
+        # 取链接参数
+        name = request.args['name']
+        pwd = request.args.get("pwd", "123456")
+        return "姓名{0}密码{1}".format(name, pwd)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="127.0.0.1", port=5000)
---
+++ common/web_flask/templates/login/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Time    : 2022/10/21 11:06 上午
+Author  : qianwulin@bytedance.com
+"""
