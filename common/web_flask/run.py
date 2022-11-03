ed8ae03c74557a2f038cda804dd0fd548764a12a
木木 <18956698335@163.com>
2022-11-03 21:54:37 +0800

    解决冲突“

--- common/web_flask/run.py
+++ common/web_flask/run.py
@@ -11,30 +11,44 @@ app.debug = True
 
 
 @app.route('/')
-def index():
-    return render_template('login/index.html')
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "GET":
+        return render_template("login.html")
     if request.method == "POST":
-        # 取表单参数
         f = request.form
         name = f["username"]
         password = f["password"]
+        user_type = f["type"]
+        print("info=", name, password, user_type)
         if name == "admin" and password == "123456":
-            return render_template('home.html', name=name)
-        # raise 主动抛出异常、abort函数，在网页当中抛出异常
-        # abort(404)
-        return "账号或者密码错误"
-    elif request.method == "GET":
-        # 取链接参数
-        name = request.args['name']
-        pwd = request.args.get("pwd", "123456")
-        return "姓名{0}密码{1}".format(name, pwd)
-    else:
-        return "请求方式不正确"
+            return render_template("user.html")
+
+
+@app.route("/test1")
+def test():
+    return "test"
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == "POST":
+#         # 取表单参数
+#         f = request.form
+#         name = f["username"]
+#         password = f["password"]
+#         if name == "admin" and password == "123456":
+#             return render_template('home.html', name=name)
+#         # raise 主动抛出异常、abort函数，在网页当中抛出异常
+#         # abort(404)
+#         return "账号或者密码错误"
+#     elif request.method == "GET":
+#         # 取链接参数
+#         name = request.args['name']
+#         pwd = request.args.get("pwd", "123456")
+#         return "姓名{0}密码{1}".format(name, pwd)
+#     else:
+#         return "请求方式不正确"
 
 
 if __name__ == '__main__':
-    app.run(host="192.168.132.1", port=808)
+    app.run(host="127.0.0.1", port=5001)
--- testcase/__init__.py
+++ testcase/__init__.py
@@ -2,3 +2,15 @@
 # @Time：2022/7/9 17:05
 # 作者：qianwulin
 
+class test:
+    def __init__(self, n):
+        self.n = n
+
+    # def __len__(self):
+    #     return len(self.n)
+
+
+if __name__ == '__main__':
+    t = test([1,2,3,4])
+    print(len(t.n))
+
